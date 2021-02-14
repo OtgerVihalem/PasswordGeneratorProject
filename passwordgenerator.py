8964221d19a1d6d75824d91ab195fe2a6d57b7fa
Otger <otgervhl@gmail.com>
2021-02-14 20:27:45 +0200

    update 14.02.2021

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -31,7 +31,7 @@ length = Spinbox(root, from_ = 8, to_ = 32 , textvariable = pass_len , width = 1
 #####define function
 
 pass_str = StringVar()
-test1="SADSADSADSADASDASD"
+passwordsafety="Parooli turvalisuse tase"
 
 def Generator():
     password = ''
@@ -43,15 +43,19 @@ def Generator():
     #password length difficulty
     x = True
     while x:
-        if (len(password) < 6 or len(password) > 12):
+        if (len(password) < 6 or len(password) > 20):
             break
         elif not re.search("[a-z]", password):
+            TestLabel['text'] = "Nõrk parool"
             break
         elif not re.search("[0-9]", password):
+            TestLabel['text'] = "Keskmine parool"
             break
         elif not re.search("[A-Z]", password):
+            TestLabel['text'] = "Tugev parool"
             break
         elif not re.search("[$#@]", password):
+            TestLabel['text'] = "Väga tugev parool"
             break
         elif re.search("\s", password):
             break
@@ -92,108 +96,9 @@ Button(root, text = 'COPY TO CLIPBOARD', command = Copy_password).pack(pady=5)
 
 ##password difficulty text box
 
-TestLabel = Label(root, text =test1, font ='arial 15 bold')
+TestLabel = Label(root, text =passwordsafety, font ='arial 15 bold')
 TestLabel.pack(pady = 6)
 
-###start of my code password length difficulty
-
-
-myGui = Tk()
-myGui.geometry('500x400+700+250')
-myGui.title('Password Generator')
-guiFont = font = dict(family='Courier New, monospaced', size=18, color='#7f7f7f')
-
-
-#====== Password Entry ==========
-eLabel = Label(myGui, text="Please Enter you Password:   ", font=guiFont)
-eLabel.grid(row=0, column=0)
-ePassword = Entry(myGui, show="*")
-ePassword.grid(row=0, column=1)
-
-
-
-#====== Strength Check =======
-
-
-def checkPassword():
-    strength = ['Password can not be Blank', 'Very Weak', 'Weak', 'Medium', 'Strong', 'Very Strong']
-    score = 1
-    password = ePassword.get()
-    password, len(password)
-
-    if len(password) == 0:
-        passwordStrength.set(strength[0])
-        return
-
-    if len(password) < 4:
-        passwordStrength.set(strength[1])
-        return
-
-    if len(password) >= 8:
-        score += 1
-
-    if re.search("[0-9]", password):
-        score += 1
-
-    if re.search("[a-z]", password) and re.search("[A-Z]", password):
-        score += 1
-
-    if re.search(".", password):
-        score += 1
-
-    passwordStrength.set(strength[score])
-
-passwordStrength = StringVar()
-checkStrBtn = Button(myGui, text="Check Strength", command=checkPassword, height=2, width=25, font=guiFont)
-checkStrBtn.grid(row=2, column=0)
-checkStrLab = Label(myGui, textvariable=passwordStrength)
-checkStrLab.grid(row=2, column=1, sticky=W)
-
-#====== Hash the Password ======
-
-
-def passwordHash():
-    hash_obj1 = hashlib.md5()
-    pwmd5 = ePassword.get().encode('utf-8')
-    hash_obj1.update(pwmd5)
-    md5pw.set(hash_obj1.hexdigest())
-
-md5pw = StringVar()
-hashBtn = Button(myGui, text="Generate Hash", command=passwordHash, height=2, width=25, font=guiFont)
-hashBtn.grid(row=3, column=0)
-hashLbl = Label(myGui, textvariable=md5pw)
-hashLbl.grid(row=3, column=1, sticky=W)
-
-
-#====== Log the Hash to a file =======
-
-
-def hashlog():
-    loghash = md5pw.get()
-
-    if os.path.isfile('password_hash_log.txt'):
-        obj1 = open('password_hash_log.txt', 'a')
-        obj1.write(loghash)
-        obj1.write("\n")
-        obj1.close()
-
-    else:
-        obj2 = open('password_hash_log.txt', 'w')
-        obj2.write(loghash)
-        obj2.write("\n")
-        obj2.close()
-
-btnLog = Button(myGui, text="Log Hash", command=hashlog, height=2, width=25, font=guiFont)
-btnLog.grid(row=4, column=0)
-
-#====== Re enter password and check against stored hash ======
-lblVerify = Label(myGui, text="Enter Password to Verify:   ", font=guiFont)
-lblVerify.grid(row=5, column=0, sticky=W)
-
-myGui.mainloop()
-
-
-#### end of my code
 
 
 # loop to run program
